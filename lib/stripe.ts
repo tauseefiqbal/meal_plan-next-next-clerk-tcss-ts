8641d63fdfd05a1f603a6a256b73cae1a2b98370
tauseefiqbal <tauseef.iqbal@live.com>
2026-06-30 05:39:01 -0600

    Updated/Added/Removed files commit

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,3 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_placeholder_for_build");
